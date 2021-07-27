d261fbc338f9a0f0f963e7c575c73242ffc5a0b4
김영채 <kevinkim2586@gmail.com>
2021-07-27 14:18:18 +0900

    init

---
+++ GitHub Followers/FavoritesListVC.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+    
+
+
+
+}
